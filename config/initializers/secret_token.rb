5d5cbdac2b1397e7d8c8def091bc70508f0e4086
drlech <drlech@gmail.com>
2013-12-20 17:51:13 +0100

    Merge?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goblin::Application.config.secret_token = 'c3399468436d5e0ea8a13146ea45011d4445b446cf292c6eaacc2cf8d37953be111f2bd76c2a1fbcf68a762aa19108e7974c0ac307aaf1da1e7daa4661ef6e89'
