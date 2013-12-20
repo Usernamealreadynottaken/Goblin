a5b1fbb1ddd3cf917dce89f0cd30cad49fb06d12
usernamealreadynottaken <maciej.laszczyk@gmail.com>
2013-12-20 17:45:02 +0100

    Initial commit2.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goblin::Application.config.secret_token = '314fc7d9ceb8c8f775b22bf8fade71a386ee1d7b7d4bcaeda8dd9922e0947fa77ca8a00f57152ec1cdf064a59ffc7faeef229b96b3740aa3016ba18397ab7680'
