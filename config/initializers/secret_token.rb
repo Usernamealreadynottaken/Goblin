583a4e980bbfca173d024ccaaeb67c069287363e
usernamealreadynottaken <maciej.laszczyk@gmail.com>
2013-12-20 18:08:19 +0100

    Gemy

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,19 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Goblin::Application.config.secret_token = '314fc7d9ceb8c8f775b22bf8fade71a386ee1d7b7d4bcaeda8dd9922e0947fa77ca8a00f57152ec1cdf064a59ffc7faeef229b96b3740aa3016ba18397ab7680'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Goblin::Application.config.secret_key = secure_token
