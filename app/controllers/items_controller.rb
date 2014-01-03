cd0bbad8bf1bb3928027a2237420f92909128f3f
drlech <drlech@gmail.com>
2014-01-03 22:02:21 +0100

    Formularz dodawania/edycji itemu: Position: absolute i wycentrowany. Jeszcze nia ma animacji. Troche js'u dopisalem do tego, na przyklad po kliknieciu 'Add item' otwiera sie czysty formularz (klikanie na item jeszcze nie jest obslugiwane).

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,29 @@
 class ItemsController < ApplicationController
   
   def add
-    render text: "Answeer"
+    render text: "Adding item!"
+    puts "Name: #{params[:item][:name]}"
+    puts "Magic name: #{params[:item][:magic_name]}"
+    puts "Properties: #{params[:item][:properties]}"
+    puts "Description: #{params[:item][:description]}"
+    if params[:item][:identified] == "1"
+      puts "Identified"
+    else
+      puts "Unidentified"
+    end
   end
   
   def update
-    print '!!! UPDATING !!!'
+    render text: "Updating item #{params[:item[:id]]}"
+    puts "Name: #{params[:item][:name]}"
+    puts "Magic name: #{params[:item][:magic_name]}"
+    puts "Properties: #{params[:item][:properties]}"
+    puts "Description: #{params[:item][:description]}"
+    if params[:item][:identified]
+      puts "Identified"
+    else
+      puts "Unidentified"
+    end
   end
   
 end
