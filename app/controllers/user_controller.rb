6f0c2fdab40fbe5646537c55952f1607f9b7be4a
usernamealreadynottaken <maciej.laszczyk@gmail.com>
2014-01-18 17:24:50 +0100

    Dodawanie friendsow, ze wstepna obsluga bledow.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -25,6 +25,31 @@ class UserController < ApplicationController
     user.update_attribute(:update_flag, false)
   end
   
+  def create_friendship
+    user = User.find(params[:id])
+    friend = User.find(:first, :conditions => [
+      'name = ?', params[:friend_name]
+    ])
+    if friend != nil
+      friendship = Friendship.find(:first, :conditions => [
+        '(user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)',
+        user.id, friend.id, friend.id, user.id
+      ])
+      result = (true if (friendship == nil)) || "You have already befriended that Nerd."
+    else
+      result = "Can't invite ghosts!"
+    end
+    
+    if (result == true)
+      Friendship.create(user_id: user.id, friend_id: friend.id, request: "pending")
+      set_friendship_flags(user, friend)
+    end
+    
+    render :json => {
+      :result => result
+    }
+  end
+  
   def remove_friendship
     render :nothing => true
     friendship = Friendship.find(:all, :conditions => [
@@ -34,6 +59,11 @@ class UserController < ApplicationController
     friendship.destroy
     user = User.find(params[:id])
     friend = User.find(params[:friend_id])
+    set_friendship_flags(user, friend)
+  end
+  
+  protected
+  def set_friendship_flags(user, friend)
     user.update_attribute(:update_flag, true)
     friend.update_attribute(:update_flag, true)
   end
